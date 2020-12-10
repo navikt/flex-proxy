554b22c2b64a8f3fe26e46771b552cde2297880a
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-12-10 16:24:07 +0100

    text plain

--- flex-spring-proxy/src/main/kotlin/no/nav/helse/flex/robots/RobotsController.kt
+++ flex-spring-proxy/src/main/kotlin/no/nav/helse/flex/robots/RobotsController.kt
@@ -2,12 +2,13 @@ package no.nav.helse.flex.robots
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 
 
 @Controller
 class RobotsController {
-    @RequestMapping("/robots.txt")
+    @RequestMapping("/robots.txt", method = [RequestMethod.GET], produces = ["text/plain"])
     @ResponseBody
     fun robots(): String {
         return """
